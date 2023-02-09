4b7ac1c0502849bd5cf4b87a0fae2c8e449d8051
uhhee12 <yunhee4560@gmail.com>
2023-02-09 13:40:55 +0900

    경로 수정

--- js/main.js
+++ js/main.js
@@ -45,3 +45,23 @@ $(document).ready(function () {
 });
 
 
+ var swiper = new Swiper('.swiper-container', {
+            slidesPerView: 3, //640~1024 해상도 외 레이아웃 뷰 개수
+            spaceBetween: 20, //위 slidesPerview 여백
+            pagination: true, // pager 여부
+            breakpoints: { //반응형 조건 속성
+                650: { //650 이상일 경우
+                    slidesPerView: 1,
+                    spaceBetween: 20
+                },
+                1024: {
+                    slidesPerView: 2,
+                    spaceBetween: 20
+                },
+            },
+            navigation: {   // 버튼 사용자 지정
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        });
+
